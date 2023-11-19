b1cd337e23cae2d421b0f7cbd4493e4f28667c5a
Glacier-elk <2191710503@qq.com>
2023-11-19 17:26:52 +0800

    LoginForm.vue UI 设计

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/main.css'
+import ElementPlus from 'element-plus';
+import 'element-plus/theme-chalk/index.css';
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
