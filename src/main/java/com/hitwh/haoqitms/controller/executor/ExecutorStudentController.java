b5479bf910c7424d9328c7a27782104ec94d5fa9
cksfafwefasdf <2318255054@qq.com>
2023-12-03 19:38:16 +0800

    完成对学生的操作

--- src/main/java/com/hitwh/haoqitms/controller/executor/ExecutorStudentController.java
+++ src/main/java/com/hitwh/haoqitms/controller/executor/ExecutorStudentController.java
@@ -1,7 +1,9 @@
 package com.hitwh.haoqitms.controller.executor;
 
 import com.hitwh.haoqitms.entity.ResultInfo;
+import com.hitwh.haoqitms.entity.Student;
 import com.hitwh.haoqitms.service.executor.ExecutorStudentService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.InputStream;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @RequestMapping("/executor/student")
 @RestController
@@ -57,6 +60,78 @@ public class ExecutorStudentController {
             resultInfo.setErrorMsg("导入失败");
         }
         return resultInfo;
+    }
+
+    /**
+     * 创建学员(前端首先要判断学员填报的团报码和公司名称是否合法)
+     * @param request
+     * @param student
+     * @return
+     */
+    @PostMapping("/create")
+    public ResultInfo create(HttpServletRequest request, @RequestBody Student student){
+        ResultInfo info = new ResultInfo();
+        try {
+            Boolean result = executorStudentService.createStudent(student);
+            if (result) {
+                info.setFlag(true);
+            } else {
+                info.setFlag(false);
+                info.setErrorMsg("创建失败");
+            }
+        } catch (Exception e) {
+            info.setFlag(false);
+            info.setErrorMsg("创建失败");
+        }
+        return info;
+    }
+
+    /**
+     * 获取所有学员的信息
+     * @param request
+     * @return
+     */
+    @GetMapping("/getAll")
+    public ResultInfo getAll(HttpServletRequest request){
+        ResultInfo info = new ResultInfo();
+        try {
+            List<Student> students = executorStudentService.getAllStudents();
+            info.setFlag(true);
+            info.setData(students);
+        }catch (Exception e){
+            e.printStackTrace();
+            info.setFlag(false);
+            info.setErrorMsg("fail to get all infos");
+        }
+        return info;
+    }
+
+    /**
+     * 更新学生信息
+     * @param student
+     */
+    @PostMapping("/update")
+    public ResultInfo updateStudent(Student student){
+        ResultInfo resultInfo = new ResultInfo();
+        if(executorStudentService.updateStudent(student)){
+            resultInfo.setFlag(true);
+        }else {
+            resultInfo.setFlag(false);
+            resultInfo.setErrorMsg("fail to update");
+        }
+        return  resultInfo;
+    }
+
+    @GetMapping("/delete")
+    public ResultInfo delete(HttpServletRequest request,@RequestParam  Integer studentId){
+        ResultInfo info = new ResultInfo();
+        if(executorStudentService.deleteStudent(studentId)){
+            info.setFlag(true);
+        }else{
+            info.setFlag(false);
+            info.setErrorMsg("fail to delete student");
+        }
+        return info;
     }
 
 }
