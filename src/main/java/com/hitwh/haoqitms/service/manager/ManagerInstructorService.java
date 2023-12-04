d23e810163c06e71da4ebcade7a86b24e78c47c4
Glacier-elk <2191710503@qq.com>
2023-12-04 11:15:10 +0800

    format

--- src/main/java/com/hitwh/haoqitms/service/manager/ManagerInstructorService.java
+++ src/main/java/com/hitwh/haoqitms/service/manager/ManagerInstructorService.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public interface ManagerInstructorService {
     @GetMapping("/total")
-    public Integer countTotalInstructor();
+    Integer countTotalInstructor();
 
 }
--- src/main/java/com/hitwh/haoqitms/service/manager/ManagerStudentService.java
+++ src/main/java/com/hitwh/haoqitms/service/manager/ManagerStudentService.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public interface ManagerStudentService {
     @GetMapping("/total")
-    public Integer countTotalStudent();
+    Integer countTotalStudent();
 
 }
