4ca684f15ee6d269878f390e834b6f57d59d3422
HITLiuJiahao <2875038450@qq.com>
2023-12-04 16:36:20 +0800

    讲师查询课程信息

---
+++ src/main/java/com/hitwh/haoqitms/service/impl/Instructor/InstructorCourseServiceImpl.java
@@ -0,0 +1,30 @@
+package com.hitwh.haoqitms.service.impl.Instructor;
+
+import com.hitwh.haoqitms.entity.Course;
+import com.hitwh.haoqitms.mapper.CourseMapper;
+import com.hitwh.haoqitms.service.Instructor.InstructorCourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InstructorCourseServiceImpl implements InstructorCourseService {
+    private final CourseMapper courseMapper;
+
+    @Autowired
+    public InstructorCourseServiceImpl(CourseMapper courseMapper) {
+        this.courseMapper = courseMapper;
+    }
+
+    /**
+     * 获取所有课程信息
+     * @param employee_id
+     * @return
+     */
+    @Override
+    public List<Course> getAllYourCourse(Integer employee_id){
+
+        return courseMapper.getAllYourCourse(employee_id);
+    }
+}
