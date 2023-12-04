e9e7a8065b24bdb3685d2746fb60136437a740bc
kurisu0306 <zh13587168037@gmail.com>
2023-12-04 10:26:44 +0800

    学员前端

--- src/main/java/com/hitwh/haoqitms/mapper/CourseApplicationMapper.java
+++ src/main/java/com/hitwh/haoqitms/mapper/CourseApplicationMapper.java
@@ -5,6 +5,9 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import org.apache.ibatis.annotations.Update;
+
+
 import java.util.List;
 
 @Mapper
@@ -15,9 +18,41 @@ public interface CourseApplicationMapper {
      * @return 是否新建成功
      */
     @Insert("insert into course_application " +
-            "(course_id, name, gender, email, company_name, job_position, skill_level, contact_info) " +
-            "values(#{courseId}, #{name}, #{gender}, #{email}, #{companyName}, #{jobPosition}, #{skillLevel}, #{contactInfo})")
+            "(course_id, course_name, name, gender, email, company_name, job_position, skill_level, contact_info) " +
+            "values(#{courseId}, #{courseName}, #{name}, #{gender}, #{email}, #{companyName}, #{jobPosition}, #{skillLevel}, #{contactInfo})")
     Boolean createCourseApplication(CourseApplication courseApplication);
+
+    /**
+     * 获取所有课程申请信息
+     */
+    @Select("select * from course_application")
+    List<CourseApplication> getAllCourseApplication();
+
+    /**
+     * 通过学员课程申请
+     */
+    @Update("update course_application set status = 1 where application_id = #{applicationId}")
+    Boolean approveApplication(Integer applicationId);
+
+    /**
+     * 拒绝学员课程申请
+     */
+    @Update("update course_application set status = 2 where application_id = #{applicationId}")
+    Boolean rejectApplication(Integer applicationId);
+
+    /**
+     * 根据申请表id获取申请表信息
+     */
+    @Select("select * from course_application where application_id = #{applicationId}")
+    CourseApplication getCourseApplicationById(Integer applicationId);
+
+    /**
+     * 根据申请表id回填学生id
+     * @param applicationId 申请表id
+     * @param studentId 学生id
+     */
+    @Update("update course_application set student_id = #{studentId} where application_id = #{applicationId}")
+    Boolean backPatchStudentId(Integer applicationId, Integer studentId);
 
     /**
      * 获取团报申请的学生列表
@@ -26,4 +61,5 @@ public interface CourseApplicationMapper {
      */
     @Select("select student_id from course_application where course_id = #{courseId} and promo_code is not null")
     List<Integer> getPromotedCourseApplications(Integer courseId);
+
 }
