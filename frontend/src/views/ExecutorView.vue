c0745d808d73db09fc844d522f0fd51d659944a9
kurisu0306 <zh13587168037@gmail.com>
2023-12-06 14:49:49 +0800

    必填项bug

--- frontend/src/views/ExecutorView.vue
+++ frontend/src/views/ExecutorView.vue
@@ -39,14 +39,12 @@ import ExecutorChangeStudentComponent from "../components/executor/ExecutorChang
 import ExecutorChangeInstructorComponent from "../components/executor/ExecutorChangeInstructorComponent.vue";
 import ExecutorInstructorCourseComponent from "../components/executor/ExecutorInstructorCourseComponent.vue";
 import {ElMessage} from "element-plus";
-import axios from "axios";
-
+import axios from "axios
 const router = useRoute();
 const activeKey = ref('1');
 const teacherId = ref('1');
 const name = ref("");
-const executorId = ref(router.query.executorId);
-
+const executorId = ref(router.query.executorId).value;
 // console.log("teacherId",teacherId.value)
 //
 function checkLogin() {
